cdbdb8f46c531323d3134cd0b81d86d2e5453868
Antoine Bellanger <dev@antoinebellanger.ch>
2019-04-08 22:49:44 +0200

    Conditional: BeginningVersionNumber, VersionNumber, FieldSizeFollowingMessage, PassengerDescription

--- Boarding Pass/BoardingPass.swift
+++ Boarding Pass/BoardingPass.swift
@@ -12,6 +12,9 @@ struct BoardingPass: Decodable {
     
     //MARK: - Mandatory Items
     
+    /// Field size of the mandatory items.
+    private let mandatoryItemsSize: Int = 60
+    
     /// Format Code of the boarding pass.
     /// Usually "M".
     let formatCode: String //M
@@ -68,8 +71,49 @@ struct BoardingPass: Decodable {
     /// Usually : 3
     let passengerStatus: Int
     
+    /// The size of the variable field. Hexadecimal converted to Int.
+    /// Example : "5D" (93), "4D" (77), "0A (10)
+    let variableSize: Int
+    
+    //MARK: - Conditional Items
+    
+    /// **Conditional** | The beginning of the version number.
+    /// Example : ">", "1"
+    let versionNumberBeginning: String
+    /// Size of the `versionNumberBeginning`.
+    private let versionNumberBeginningSize: Int = 1
+    /// Size of the combined previous conditional data.
+    private let versionNumberBeginningConditionalSize: Int = 0
+    
+    /// **Conditional** | The version number.
+    /// Example : "2", "5", "8"
+    let versionNumber: String
+    /// Size of the `versionNumber`.
+    private let versionNumberSize: Int = 1
+    /// Size of the combined previous conditional data.
+    private let versionNumberConditionalSize: Int = 1
+    
+    /// **Conditional** | The field size of the following structured message.
+    /// Count for the length of the conditional data identified as unique (the 7 next variables)
+    /// Example : "18"
+    let followingStructureMessageSize: Int
+    /// Size of the `followingStructureMessageSize`.
+    private let followingStructureMessageSizeSize: Int = 2
+    /// Size of the combined previous conditional data.
+    private let followingStructureMessageSizeConditionalSize: Int = 2
+    
+    /// **Conditional** | The description of the passenger.
+    /// Example : "0" (adult), "4" (infant), "6" (adult with an infant)
+    let passengerDescription: Int
+    /// Size of the `passengerDescription`.
+    private let passengerDescriptionSize: Int = 1
+    /// Size of the combined previous conditional data.
+    private let passengerDescriptionConditionalSize: Int = 4
+    
     //MARK: - Initialisation
     init(from data: String) throws {
+        
+        //Mandatory
         
         //Format Code
         self.formatCode = data[0].string
@@ -115,6 +159,55 @@ struct BoardingPass: Decodable {
         
         //Passenger Status
         self.passengerStatus = Int(data[57].string) ?? 0
+        
+        //Variable size
+        let fieldSize = data[58...59].string
+        guard let value = UInt8(fieldSize, radix: 16) else {
+            throw BoardingPassError.invalidHexadecimal
+        }
+        self.variableSize = Int(value)
+        
+        //Conditional
+        
+        //Version number beginning
+        if self.variableSize > (versionNumberBeginningConditionalSize + versionNumberBeginningSize) {
+            self.versionNumberBeginning = data[mandatoryItemsSize + versionNumberBeginningConditionalSize].string
+        } else {
+            self.versionNumberBeginning = ""
+        }
+        
+        //Version number
+        if self.variableSize > (versionNumberConditionalSize + versionNumberSize) {
+            self.versionNumber = data[mandatoryItemsSize + versionNumberConditionalSize].string
+        } else {
+            self.versionNumber = "0"
+        }
+        
+        //Following structure message size
+        if self.variableSize > (followingStructureMessageSizeConditionalSize + followingStructureMessageSizeSize) {
+            let startIndex = mandatoryItemsSize + followingStructureMessageSizeConditionalSize
+            let endIndex = mandatoryItemsSize + followingStructureMessageSizeConditionalSize + followingStructureMessageSizeSize - 1
+            let messageSize = data[startIndex...endIndex].string
+            print(messageSize)
+            guard let value = UInt8(messageSize, radix: 16) else {
+                throw BoardingPassError.invalidHexadecimal
+            }
+            self.followingStructureMessageSize = Int(value)
+        } else {
+            self.followingStructureMessageSize = 0
+        }
+
+        //Passenger description
+        if self.variableSize > (passengerDescriptionConditionalSize + passengerDescriptionSize) {
+            let index = mandatoryItemsSize + passengerDescriptionConditionalSize
+            guard data[index].string.isInt else {
+                throw BoardingPassError.invalidPassengerDescription
+            }
+            self.passengerDescription = Int(data[index].string)!
+        } else {
+            self.passengerDescription = 99
+        }
+        
     }
     
 }
@@ -122,6 +215,8 @@ struct BoardingPass: Decodable {
 enum BoardingPassError: Error {
     
     case invalidLegsNumber
+    case invalidHexadecimal
+    case invalidPassengerDescription
 }
 
 // Extensions for the substring
